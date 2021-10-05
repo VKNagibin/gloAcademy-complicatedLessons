f678083b6b9e7153f750d581e39cfec5802bdc02
Vyacheslav Nagibin <itisnotstupid@gmail.com>
2021-10-05 14:03:54 +0300

    lesson03 add array

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ if (question){
     lang = 'en';
 }
 
+let num = {ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+           en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+          };
+console.log(num[lang]);
+
+if (lang =='ru') {
+    console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
+} else {
+    console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+}
 switch (true) {
     case lang =='ru':
         console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
