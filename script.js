65af1007d915c93764c6823839f735d764524a41
Vyacheslav Nagibin <itisnotstupid@gmail.com>
2021-10-07 14:07:48 +0300

    lesson06

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 "use strict";
 
-const difficultTask1 = function() {
+const gameBegin = function(theNumber) {
 
-let checkNumber;
+    let count = 10;
 
-const isNumber = function(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
+    return function closure() {
+
+        if (count >= 1) {
+
+            let check = prompt("Угадай число от 1 до 100");
+            let userNumber = (check === null || check.trim() == '') ? NaN : +check ;
+
+            if (typeof userNumber == 'number' && !isNaN(userNumber)) {
+                
+                switch (true) {
+                    case (userNumber > theNumber):
+                        alert(`Загаданное число меньше, осталось попыток ${--count}`);
+                        closure();
+                        break;
+                    case (userNumber < theNumber):
+                        alert(`Загаданное число больше, осталось попыток ${--count}`);
+                        closure();
+                        break;
+                    case (userNumber == theNumber):
+                        confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")  ? gameBegin( Math.round((Math.random() * 100) + 1) )() : '';
+                }
+
+            } else if (check === null) {
+                alert("Игра окончена");
+            } else {
+                let i = confirm("Введи число");
+                (i === false) ? alert('игра окончена'): closure();
+            }
+
+        } else {
+            console.log(theNumber);
+            confirm("Попытки закончились, хотите сыграть еще?") ? gameBegin( Math.round((Math.random() * 100) + 1) )() : '';
+        }
+
+
+    };
 };
 
-do {
-    checkNumber = prompt('Введите число');
-} while (!isNumber(checkNumber));
+let game = gameBegin( Math.round((Math.random() * 100) + 1) );
 
-checkNumber = +checkNumber.toString().trim();
-console.log(typeof checkNumber, checkNumber);
-
-};
-
-difficultTask1();
-
+game();
