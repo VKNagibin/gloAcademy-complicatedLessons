1a6770757b228424df8b9a69cc1054a56c7f30f1
Vyacheslav Nagibin <itisnotstupid@gmail.com>
2021-10-09 16:02:21 +0300

    lesson07-fixed

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 'use strict';
 
 let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-
-const body = document.querySelector('body');
-const div = document.createElement('div');
 let currentDay = new Date().getDay();
 let log ='';
+let sunday;
+const body = document.querySelector('body');
+const div = document.createElement('div');
 
 week[currentDay] = week[currentDay].bold();
 week[0] = week[0].italics();
 week[week.length - 1] = week[week.length - 1].italics();
+sunday = week.shift();
+week.push(sunday);
 
 for ( let elem in week) {
     log += `<p style="width: 200px; text-align: center; margin: 0 auto">${week[elem]}</p>`;
